96da06c5b6d32295653b0ee040e338e0deb0af09
lfkdsk <lfk_dsk@hotmail.com>
2018-10-07 21:04:45 +0800

    add yapypy debug method

--- yapypy/utils/easy_debug.py
+++ yapypy/utils/easy_debug.py
@@ -8,6 +8,20 @@ from yapypy.extended_python.py_compile import py_compile
 from yapypy.extended_python.symbol_analyzer import to_tagged_ast
 
 
+def yapypy_debug(code: str, should_exec=False, ctx=None):
+    res = to_tagged_ast(parse(code).result)
+    c = py_compile(res)
+    print("-----------Code")
+    print(code)
+    print("-----------YaPyPy")
+    print(dis.dis(c))
+    print("-----------YaPyPy exec result")
+    if should_exec:
+        exec(c, ctx or {})
+    else:
+        print("\t(skip)")
+
+
 def easy_debug(code: str, should_exec=False, ctx=None):
     res = to_tagged_ast(parse(code).result)
     c = py_compile(res)
